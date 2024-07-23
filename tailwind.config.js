9f148d8fdeb4f955bc5f5809cbd57aaa56fc1057
bertbrian <bertbrian97@gmail.com>
2024-07-23 21:43:13 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens : {
+      sm: '480px',
+      md: '768px',
+      lg: '1020px',
+      xl: '1440px',
+    },
+    extend: {
+      colors:{
+      
+      },
+      fontFamily:{
+        sans: ['Montserrat','sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
