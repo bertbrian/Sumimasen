4ccee16c578d5b87196e24ec13ca409c9e0478e5
Albert Brian <bertbrian97@gmail.com>
2024-08-07 01:12:14 +0700

    Add Certification & About Us

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,10 @@ module.exports = {
   ],
   theme: {
     screens : {
-      sm: '390px',
+      sm: '480px',
       md: '768px',
-      lg: '1020px',
-      xl: '1440px',
+      lg: '1024px',
+      xl: '1200px',
     },
     extend: {
       colors:{
