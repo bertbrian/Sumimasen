959a68cf74302517b30755ba90705e3ed21cb98e
Albert Brian <bertbrian97@gmail.com>
2024-10-13 13:32:57 +0700

    pindahin ke section

--- js/script.js
+++ js/script.js
@@ -1,21 +1,16 @@
-const btn = document.getElementById('menu-btn')
-const menu = document.getElementById('menu')
-const btncampaign = document.getElementById('campaign')
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
 
-btn.addEventListener('click', navToggle)
-menu.addEventListener('click', menuclicked)
+btn.addEventListener("click", navToggle);
+menu.addEventListener("click", menuclicked);
 
 //Toggle Mobile Menu
 function navToggle() {
-    btn.classList.toggle('open')
-    menu.classList.toggle('flex')
-    menu.classList.toggle('hidden')
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
 }
 
-function menuclicked(){
-    navToggle()
+function menuclicked() {
+  navToggle();
 }
-
-window.addEventListener('load', () => {
-    btncampaign.click();
-})
